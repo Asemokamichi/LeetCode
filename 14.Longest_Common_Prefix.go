2dfe9e617a2a6d07acf226c50fb4d1ec1f5e0280
ASSEM MALIKOVA <100547586+Asemokamichi@users.noreply.github.com>
2022-04-09 15:10:36 +0600

    Update 14.Longest_Common_Prefix.go

--- 14.Longest_Common_Prefix.go
+++ 14.Longest_Common_Prefix.go
@@ -1,3 +1,7 @@
+/*Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".*/
+
 package main
 
 import "fmt"
@@ -48,7 +52,7 @@ func min_arr(arr []string) (string, int) {
 }
 
 func check_toFind(str, toFind string) bool {
-	if str[0:len(toFind)] == toFind {
+	if str[:len(toFind)] == toFind {
 		return true
 	}
 	return false
